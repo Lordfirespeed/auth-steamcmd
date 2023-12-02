9826153cbff2019202c6ce27cd7a5365f43f84bf
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-12-02 07:50:19 +0000

    re-export libs from index files

---
+++ src/lib/decorator/index.ts
@@ -0,0 +1,1 @@
+export { default as ActionLogGroup } from "./action.log.group";
---
+++ src/lib/index.ts
@@ -0,0 +1,4 @@
+export * from "./decorator"
+export { default as discernFileSystemErrorReason } from "./file.system.error.reason"
+export { default as discernActionInputErrorReason } from "./action.input.error.reason"
+export { default as isErrnoException } from "./is.errno.exception"
