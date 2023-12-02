814260a28564a449837d425ecc269b26e675b440
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-12-02 07:51:46 +0000

    call main entrypoint from index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 /**
  * The entrypoint for the action.
  */
-import { run } from './main'
+import * as core from '@actions/core'
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-run()
+import { AuthenticateSteamCMD } from './main'
+
+async function wrap_install() {
+  await (new AuthenticateSteamCMD).run().catch(error => core.setFailed(error))
+}
+
+void wrap_install()
