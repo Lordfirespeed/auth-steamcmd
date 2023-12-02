a6a141317796f46d07ec4e2aadc8b80720ea3cf6
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-12-02 23:53:04 +0000

    update entry-point test

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -2,16 +2,39 @@
  * Unit tests for the action's entrypoint, src/index.ts
  */
 
-import * as main from '../src/main'
+import AuthenticateSteamCMD from '../src/main'
+import * as core from '@actions/core'
 
 // Mock the action's entrypoint
-const runMock = jest.spyOn(main, 'run').mockImplementation()
+const mockRun = jest.fn()
+
+// Mock the 'main' module
+jest.mock("../src/main", () => {
+  return jest.fn().mockImplementation(() => {
+      return { run: mockRun }
+    })
+})
+
+// Mock the GitHub Actions core library
+let setFailedMock: jest.SpyInstance
 
 describe('index', () => {
-  it('calls run when imported', async () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+
+    setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
+  })
+
+  it('calls run when imported and reports failure when an error is thrown', async () => {
+    const runComplete = Promise.reject();
+    mockRun.mockImplementation(() => runComplete)
+
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     require('../src/index')
+    await runComplete.catch(() => { return });
 
-    expect(runMock).toHaveBeenCalled()
+    expect(AuthenticateSteamCMD).toHaveBeenCalled()
+    expect(mockRun).toHaveBeenCalled()
+    expect(setFailedMock).toHaveBeenCalled()
   })
 })
