7e99e00352e1f34cd4237845274273464d8a1ace
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-12-02 07:48:36 +0000

    declare dev dependency on type defns for is-base64

---
+++ src/lib/action.input.error.reason.ts
@@ -0,0 +1,12 @@
+type actionInputErrorContext = {
+  inputKey: string
+}
+
+export default function discernActionInputErrorReason(error: unknown, context: actionInputErrorContext): string {
+  if (!(error instanceof Error)) {
+    if (error instanceof String) return `Unknown: ${error}}`
+    return "Unknown: thrown value not an Error"
+  }
+
+  return `${error.name}: ${error.message}`
+}
