fe267402bcabe15cea12956a64a2db0e792c43b9
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-12-02 07:50:05 +0000

    add decorator for grouping method logs

---
+++ src/lib/decorator/action.log.group.ts
@@ -0,0 +1,18 @@
+import * as core from "@actions/core"
+
+export default function ActionLogGroup(groupTitle: string) {
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const oldFunction = descriptor.value;
+
+    descriptor.value = function(...args: unknown[]) {
+      core.startGroup(groupTitle)
+      try {
+        return oldFunction.apply(target, args);
+      }
+      finally {
+        core.endGroup()
+      }
+    }
+  }
+}
