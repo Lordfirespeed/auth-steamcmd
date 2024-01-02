974f5b9719e75a541b86d90dc3924352d0afa719
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-01-02 17:18:10 +0000

    fix lint problems

--- src/lib/expand-env.ts
+++ src/lib/expand-env.ts
@@ -1,5 +1,5 @@
 // https://github.com/taterbase/node-expandenv/blob/56ec358f/index.js#L3
-export default function expandEnv(value: string, env: Map<string, string> | undefined = undefined) {
+export default function expandEnv(value: string, env: Map<string, string> | undefined = undefined): string {
   const mergedEnv = mapUnion(new Map(), env?.entries() || [])
 
   return value.replace(/\$\w+/g, match => {
@@ -8,7 +8,7 @@ export default function expandEnv(value: string, env: Map<string, string> | unde
 }
 
 // https://stackoverflow.com/a/41328397/11045433
-function mapUnion<K, V>(map: Map<K, V>, ...iterables: (Iterable<[K, V]> | Array<[K, V]>)[]) {
+function mapUnion<K, V>(map: Map<K, V>, ...iterables: (Iterable<[K, V]> | [K, V][])[]): Map<K, V> {
   for (const iterable of iterables) {
     for (const item of iterable) {
       map.set(...item)
