71f14e61dbd71990536c2b3739394be65a7a9ac0
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-01-02 17:18:27 +0000

    fix lint problems

--- src/lib/action-input-error-reason.ts
+++ src/lib/action-input-error-reason.ts
@@ -2,7 +2,7 @@ type actionInputErrorContext = {
   inputKey: string
 }
 
-export default function discernActionInputErrorReason(error: unknown, context: actionInputErrorContext): string {
+export default function discernActionInputErrorReason(error: unknown, _context: actionInputErrorContext): string {
   if (!(error instanceof Error)) {
     if (error instanceof String) return `Unknown: ${error}}`
     return 'Unknown: thrown value not an Error'
