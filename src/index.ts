d9ab56a35243afdc3aa0d7ca667ffb2eba277ed2
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-01-02 22:11:05 +0000

    export the wrap_install function for testing

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import * as core from '@actions/core'
 import AuthenticateSteamCMD from './main'
 import typeSafeError from './lib/type-safe-error'
 
-async function wrap_install(): Promise<void> {
+export default async function wrap_install(): Promise<void> {
   try {
     await new AuthenticateSteamCMD().run()
   } catch (error) {
